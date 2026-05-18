d5ceb84aca827f2bb3f63eec1aaf68d5e8dba28d
Jadel Bader <jadelbader123@gmail.com>
2026-05-19 02:46:16 +0300

    Add Book and Review models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.TextField()
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title}"
